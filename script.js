9859a5f9af41ba5ec6be33410d4406b21a8b2fd3
Akutsang <mymac@MYs-MacBook-Pro.local>
2022-12-16 01:26:28 +0100

    query selector for player, computer, userscore,computerscore and round updated

---
+++ script.js
@@ -0,0 +1,6 @@
+const player = document.querySelector("#user");
+const computer = document.querySelector("#computer");
+const userScore = document.querySelector("#user-score");
+const computerScore = document.querySelector("#computer-score");
+const round = document.querySelector("#round");
+
