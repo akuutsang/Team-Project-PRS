1acb77af78424189dc7de95d4a241a0ba694268d
Akutsang <mymac@MYs-MacBook-Pro.local>
2022-12-16 01:28:54 +0100

    array for choices updated and function computer selection updated

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const userScore = document.querySelector("#user-score");
 const computerScore = document.querySelector("#computer-score");
 const round = document.querySelector("#round");
 
+const choice = ["Paper", "Rock", "Scissors"];
+
+function computerSelection () {
+    return choice[Math.floor(Math.random()*choice.length)];
+}
+// console.log(computerSelection());
