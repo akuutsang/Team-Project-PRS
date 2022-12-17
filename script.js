1ede463abb720989da1ef7af986b0782885a3e23
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-12-17 12:02:27 +0100

    Declare a lastMove variable to taarget ID instruction

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ const paper = document.querySelector("#paper");
 const rock = document.querySelector("#rock");
 const scissors = document.querySelector("#scissors");
 const result = document.querySelector("#result").innerHTML;
-console.log(result);
+const lastMove = document.querySelector("#instruction").innerHTML;
 
 function computerSelection () {
     const choice = ["Paper", "Rock", "Scissors"];
     return choice[Math.floor(Math.random()*choice.length)];
 }
-console.log(computerSelection());
 
 
 paper.addEventListener("click", function(){
