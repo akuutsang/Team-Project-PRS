acb2d6978452fb3a2002e2ffdf4c355efba5a09d
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-12-17 12:33:19 +0100

    Create a round fuction to compare computer and user choices

--- script.js
+++ script.js
@@ -15,16 +15,59 @@ function computerSelection () {
     return choice[Math.floor(Math.random()*choice.length)];
 }
 
+//PlayRound function to play rounds of the game and accept user and computer choices
+function playRound(userChoice){
+    const computerChoice = computerSelection();
+    const userTag = "User".fontsize(3).sub();
+    const computerTag = "Comp".fontsize(3).sub();
+    switch (userChoice + computerChoice){
+        case "PaperRock":
+            result.innerHTML = `${userChoice}${userTag} Covers ${computerTag}${computerChoice}.. You Won This Round!🎇`
+
+        case "RockScissors":
+            result.innerHTML = `${userChoice}${userTag} Smashes ${computerTag}${computerChoice}.. You Won This Round!🎇`
+            
+        case "ScissorsPaper":
+            result.innerHTML = `${userChoice}${userTag} Cuts ${computerTag}${computerChoice}.. You Won This Round!🎇`
+            break;
+
+        case "RockPaper":
+            result.innerHTML = `${computerChoice}.${computerTag} Covers ${userTag}${userChoice}. You Lose This Round!😒`
+
+        case "ScissorsRock":
+            result.innerHTML = `${computerChoice}.${computerTag} Smashes ${userTag}${userChoice}. You Lose This Round!😒`
+
+        case "PaperScissors":
+            result.innerHTML = `${computerChoice}.${computerTag} Cuts ${userTag}${userChoice}. You Lose This Round!😒`
+            break;
+
+        case "RockRock":
+            result.innerHTML = `${userChoice}${userTag} Equals ${computerTag}${computerChoice}..This Round is a Tie!😕`;
+            
+        case "ScissorsScissors":
+            result.innerHTML = `${userChoice}${userTag} Equals ${computerTag}${computerChoice}.This Round is a Tie!😕`;
+
+        case "PaperPaper":
+            result.innerHTML = `${userChoice}${userTag} Equals ${computerTag}${computerChoice}..This Round is a Tie!😕`;
+            break; 
+    }
+    console.log(userChoice);
+    console.log(computerChoice);
+}
+
 //Function to initiate user selection
 function userSelection(){
     paper.addEventListener("click", function(){
+        playRound("Paper");
         lastMove.innerHTML = `Your Last Move Was Paper 🖐️`;
     });
 
     rock.addEventListener("click", function(){
+        playRound("Rock");
         lastMove.innerHTML = `Your Last Move Was Rock ✊`;    
     });
     scissors.addEventListener("click",function(){
+        playRound("Scissors");
         lastMove.innerHTML = `Your Last Move Was Scissors ✌️`;
     } );
 }
